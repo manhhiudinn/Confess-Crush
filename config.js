f055a48b6d5fb18f9c0f02796c5c00cee1ec2ed8
manhhiudinn <94582940+manhhiudinn@users.noreply.github.com>
2021-11-18 09:23:06 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,14 @@
 const CONFIG = {
     titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+    introTitle: 'Dinnn',
+    introDesc: `Hong biết nà em bé còn zận emm hongggg taaaa`,
+    btnIntro: 'Aluu babyy',
+    title: 'Mỏninnnn bé iuuu 🥰',
+    desc: 'Bé có iu em honggg ',
+    btnYes: 'iuuu nắmm nunn 💞',
+    btnNo: 'hok iuu 😡',
+    question: 'Chọn iuu em zayy thì bé đừn zận nựaaa nkaaa 🥺',
+    btnReply: 'Gửi cho emm',
+    reply: 'đượt ruii anh hok zận emm nữa, anh iu emm baby ❤️',
+    mess: 'aww 🥰 iuu em bé nắmmmm 😘😘',
+    messDesc: 'Em yêu anh ❤️',
